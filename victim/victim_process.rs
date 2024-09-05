46e79a18f53b5672f7b18e486b02ece76f65652d
HackTestes <caio.hacktestes@gmail.com>
2024-09-05 17:44:25 -0300

    Added logic to conditionally compile the types of memory sections

--- victim/victim_process.rs
+++ victim/victim_process.rs
@@ -8,11 +8,33 @@ fn main()
 {
     println!("PID: {}", process::id());
 
-    let mut num: u32 = 0;
-    let mut vector: Vec<u8> = vec![0; 1024];
-    let vector_len: usize = vector.len();
-    vector[vector_len-1] = 1;
 
+    // Contiguous section
+    // rustc --cfg 'mem="\cm\"' (Windows)
+    #[cfg(mem="cm")]
+    {
+        println!("Contiguous memory");
+        let mut vector: Vec<u8> = vec![1; 8*1024*1024*1024];
+        let vector_len: usize = vector.len();
+        vector[vector_len-1] = 2;
+    }
+
+
+    // Non contiguous section
+    // rustc --cfg 'mem="\ncm\"' (Windows)
+    #[cfg(mem="ncm")]
+    {
+        println!("Non-contiguous memory");
+        let mut vector: Vec<Vec<u8>> = vec![];
+        for _ in 0..16
+        {
+            vector.push( vec![1; 512*1024*1024] );
+        }
+    }
+
+
+
+    let mut num: u32 = 0;
     loop
     {
         num += 1;
