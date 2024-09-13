197eb9e5615c018a5cf9ecd79048b6b3ca3697b9
HackTestes <caio.hacktestes@gmail.com>
2024-09-13 11:20:19 -0300

    Escape char on double commas was wrong. Refactored in a way that the vectors wouldn't be dropped when reaching the number loop

--- victim/victim_process.rs
+++ victim/victim_process.rs
@@ -4,36 +4,8 @@
 use std::io;
 use std::process;
 
-fn main()
+fn number_loop()
 {
-    println!("PID: {}", process::id());
-
-
-    // Contiguous section
-    // rustc --cfg 'mem="\cm\"' (Windows)
-    #[cfg(mem="cm")]
-    {
-        println!("Contiguous memory");
-        let mut vector: Vec<u8> = vec![1; 8*1024*1024*1024];
-        let vector_len: usize = vector.len();
-        vector[vector_len-1] = 2;
-    }
-
-
-    // Non contiguous section
-    // rustc --cfg 'mem="\ncm\"' (Windows)
-    #[cfg(mem="ncm")]
-    {
-        println!("Non-contiguous memory");
-        let mut vector: Vec<Vec<u8>> = vec![];
-        for _ in 0..16
-        {
-            vector.push( vec![1; 512*1024*1024] );
-        }
-    }
-
-
-
     let mut num: u32 = 0;
     loop
     {
@@ -42,5 +14,38 @@ fn main()
         println!("Num: {} -- Address: {:p}", num, &num);
 
         io::stdin().read_line(&mut guess).expect("failed to readline");
+    }
+}
+
+fn main()
+{
+    println!("PID: {}", process::id());
+
+
+    // Contiguous section
+    // rustc --cfg 'mem=\"cm\"' (Windows)
+    #[cfg(mem="cm")]
+    {
+        println!("Contiguous memory");
+        let mut vector: Vec<u8> = vec![1; 8*1024*1024*1024];
+        let vector_len: usize = vector.len();
+        vector[vector_len-1] = 2;
+
+        number_loop();
+    }
+
+
+    // Non contiguous section
+    // rustc --cfg 'mem=\"ncm\"' (Windows)
+    #[cfg(mem="ncm")]
+    {
+        println!("Non-contiguous memory");
+        let mut vector: Vec<Vec<u8>> = vec![];
+        for _ in 0..16
+        {
+            vector.push( vec![1; 512*1024*1024] );
+        }
+
+        number_loop();
     }
 }
